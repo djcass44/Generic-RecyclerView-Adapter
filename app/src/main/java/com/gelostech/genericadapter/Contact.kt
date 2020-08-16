47ce5cc2f96b92aa9d3b157ba3b620c9446b56ab
Django Cass <django@dcas.dev>
2020-08-16 19:51:59 +1000

    Replaced manual item casting with Generics

--- app/src/main/java/com/gelostech/genericadapter/Contact.kt
+++ app/src/main/java/com/gelostech/genericadapter/Contact.kt
@@ -1,7 +1,6 @@
 package com.gelostech.genericadapter
 
 data class Contact(
-        var name: String? = null,
-        var phone: String? = null
-) {
-}
+    var name: String? = null,
+    var phone: String? = null
+)
--- genericrecycleradapter/src/main/java/com/gelostech/genericrecycleradapter/RecyclerViewAdapter.kt
+++ genericrecycleradapter/src/main/java/com/gelostech/genericrecycleradapter/RecyclerViewAdapter.kt
@@ -9,21 +9,21 @@ import android.view.ViewGroup
  *  @param layout -> The item layout to be inflated
  *  @param callbacks -> The AdapterCallback interface which is used to set data to the UI and handle view clicks
  */
-class RecyclerViewAdapter(val layout: Int, private val callbacks: AdapterCallback): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-    private val items = mutableListOf<Any>()
+class RecyclerViewAdapter<T>(private val layout: Int, private val callbacks: AdapterCallback<T>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder<T>>() {
+    private val items = mutableListOf<T>()
 
     /**
      *  Function to add single item to the RecyclerView
      */
-    fun addItem(item: Any) {
-        this.items.add(item)
-        notifyItemInserted(items.size-1)
+    fun addItem(item: T) {
+        items.add(item)
+        notifyItemInserted(items.size - 1)
     }
 
     /**
      *  Function to add a list of items to the RecyclerView
      */
-    fun addItems(items: List<Any>) {
+    fun addItems(items: List<T>) {
         this.items.addAll(items)
         notifyDataSetChanged()
     }
@@ -32,15 +32,15 @@ class RecyclerViewAdapter(val layout: Int, private val callbacks: AdapterCallbac
      *  Function to clear the RecyclerView
      */
     fun clearItems() {
-        this.items.clear()
+        items.clear()
         notifyDataSetChanged()
     }
 
     /**
      *  Function to inflate the layout item
      */
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        return ViewHolder(p0.inflate(layout), callbacks)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder<T> {
+        return ViewHolder(viewGroup.inflate(layout), callbacks)
     }
 
     /**
@@ -51,19 +51,21 @@ class RecyclerViewAdapter(val layout: Int, private val callbacks: AdapterCallbac
     /**
      *  Function to bind each item in the items list to the RecyclerView at a given position
      */
-    override fun onBindViewHolder(holder: ViewHolder, pos: Int) {
+    override fun onBindViewHolder(holder: ViewHolder<T>, pos: Int) {
         holder.bind(items[pos], pos)
     }
 
     /**
      *  The ViewHolder class which binds the data to the RecyclerView item
      */
-    class ViewHolder(private val view: View, private val callbacks: AdapterCallback) : RecyclerView.ViewHolder(view) {
+    class ViewHolder<T>(private val view: View, private val callbacks: AdapterCallback<T>): RecyclerView.ViewHolder(view) {
 
-        fun bind(item: Any, position: Int) {
+        fun bind(item: T, position: Int) {
             callbacks.bindViews(view, item, position)
 
-            view.setOnClickListener { callbacks.onViewClicked(view, item) }
+            view.setOnClickListener {
+                callbacks.onViewClicked(view, item)
+            }
         }
 
     }
